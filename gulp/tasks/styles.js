370d2dbbac89d47823c32984a65bf2d3f1d13fc8
Aleksander Lamkov <aleksanderlamkov@gmail.com>
2021-07-24 15:15:09 +0500

    v 2.0

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,13 +4,13 @@ import config from '../config';
 import sourcemaps from 'gulp-sourcemaps';
 import concat from 'gulp-concat';
 
-export const stylesBuild = (isBuild = true) => (
+export const stylesBuild = () => (
   gulp.src(`${config.src.postcss}/style.pcss`)
     .pipe(sourcemaps.init())
-    .pipe(postcss({ isBuild: isBuild }))
+    .pipe(postcss({ isBuild: true }))
     .pipe(concat('style.min.css'))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(config.dest.css))
 );
 
-export const stylesWatch = () => gulp.watch(`${config.src.postcss}/**/*.pcss`, stylesBuild(false));
+export const stylesWatch = () => gulp.watch(`${config.src.postcss}/**/*.pcss`, stylesBuild);
